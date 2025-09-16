dd99d5fd88d79c824493ae4018962ab8a717ad7a
clement poissonnet <clement.poissonnet@gmail.com>
2025-09-16 16:39:04 +0200

    debbug

--- main.py
+++ main.py
@@ -211,7 +211,7 @@ if city:
                 hover_event=False,
                 override_height=500,
                 override_width="100%",
-                key="weather_plot"
+                key="weather_plot_filtered"
             )
                 
 
