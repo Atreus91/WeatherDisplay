de7c3cbd5e542a7b91f6c93b0d7e602f3f46a17c
clement poissonnet <clement.poissonnet@gmail.com>
2025-09-23 18:51:16 +0200

    port

--- main_dash.py
+++ main_dash.py
@@ -197,4 +197,5 @@ def update_graph(clickData, df_records, stored_day):
 
 # Run app
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 8050))  # Render te donne le port via la variable d'environnement PORT
+    app.run(debug=True, host="0.0.0.0", port=port)
